8a4a44403f367de231eeb034994f06551345bada
victoriabaik <eunseo@uw.edu>
2017-01-19 15:23:01 -0800

    Participation

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,31 +1,34 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame("USPersonalExpenditure")
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
-
+us.personal.expenditure <- data.frame("USPersonalExpenditure")
+View("USPersonalExpenditure")
 # What are the column names of your dataframe?
+colnames(us.personal.expenditure)
 
+# getwd()
+# setwd("~/Desktop")
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(us.personal.expenditure)
 
 # Create a column "category" that is equal to your rownames
-
+us.personal.expenditure$category <- rownames(us.personal.expenditure)
 
 # How much money was spent on personal care in 1940?
-
+us.personal.expenditure[1940,'personal care']
 
 # How much money was spent on Food and Tobacco in 1960?
 
